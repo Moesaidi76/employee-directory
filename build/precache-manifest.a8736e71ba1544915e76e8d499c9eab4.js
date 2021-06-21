d62805e62d407864bff9d50dd44791178149f0d8
Mohamed Saidi <mohamedsaidi76@gmail.com>
2021-06-21 09:03:00 -0400

    Corrected files

---
+++ build/precache-manifest.a8736e71ba1544915e76e8d499c9eab4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99fda8ca42432c7a7575208d798bc153",
+    "url": "/index.html"
+  },
+  {
+    "revision": "73eced61f4fe46c0b804",
+    "url": "/static/css/main.03bd0d11.chunk.css"
+  },
+  {
+    "revision": "dfcf0dc96d1724c0c9ff",
+    "url": "/static/js/2.7f0e3670.chunk.js"
+  },
+  {
+    "revision": "849657740f66d00720db130cc1e94c97",
+    "url": "/static/js/2.7f0e3670.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73eced61f4fe46c0b804",
+    "url": "/static/js/main.0435a870.chunk.js"
+  },
+  {
+    "revision": "c74b6dcb725f329d7bfe",
+    "url": "/static/js/runtime-main.403e0656.js"
+  }
+]);
---
+++ src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/Footer.css"
+
+function Footer() {
+    return (
+      <footer className="bg-dark footer text-white">
+        <div className="card-header bg-dark text-white">
+        &copy; 2021 Moe Saidi
+        </div>
+
+      </footer>
+    );
+  }
+  
+  export default Footer;
+  
